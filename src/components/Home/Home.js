b105357febe42c941ed9fe815a83bcea11249432
mayankmj25 <250900mj@gmail.com>
2024-01-08 20:04:11 +0530

    created homepage, will need to improve

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import Me from '../../assets/me.jpeg'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home' id='home'>
+      <div className="left">
+        <div className="profile-pic">
+          <img src={Me} alt='me'/>
+        </div>
+      </div>
+      <div className="right">
+        <div className="container"></div>
+        <h1>
+          Mayank  Jaiswal
+        </h1>
+        <span>Frontend Developer</span>
+        <div className="about-text">
+        Mayank Jaiswal is a Software Developer with experience in Tekion Corp and Synaptic. He has worked on React, React Native, Typescript and Javascript specifically. In his free time, he loves to watch football.
+      </div>
+      <div className='roles'>
+        <span onClick={() => window.open("https://github.com/mayankmj25")}>
+          <i className='fa-brands fa-square-github fa-2x fa-beat' href="www.google.com">
+          </i>
+        </span>
+        <span onClick ={() => window.open("https://www.linkedin.com/in/mayankjaiswal25")}>
+        <i className="fa-brands fa-linkedin fa-2x fa-beat"></i>
+        </span>
+      </div>
+      </div>
+    </div>
   )
 }
 
