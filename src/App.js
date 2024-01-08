470408b08665f66bbcbe2d5de3c98e01ea20f00f
mayankmj25 <250900mj@gmail.com>
2024-01-08 13:28:12 +0530

    created basic folder structure, can move to design individual component and adding data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+import About from './components/Aboutme/About'
+import Projects from './components/Projects/Projects'
 
 function App() {
   return (
     <div className="App">
-      My Portfolio Site
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
     </div>
   );
 }
